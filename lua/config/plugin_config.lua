9a20c1d0cc77ac434b553e3154a0e4f6e9934efd
Stan <zyyu@umich.edu>
2026-05-20 09:03:27 -0400

    revert fff

--- lua/config/plugin_config.lua
+++ lua/config/plugin_config.lua
@@ -114,35 +114,36 @@ if conform_ok and not is_vscode then
 end
 
 -- fff
-vim.api.nvim_create_autocmd("PackChanged", {
-    callback = function(ev)
-        local name, kind = ev.data.spec.name, ev.data.kind
-        if name == "fff.nvim" and (kind == "install" or kind == "update") then
-            if not ev.data.active then
-                vim.cmd.packadd("fff.nvim")
+local fff_ok, _ = pcall(function()
+    vim.api.nvim_create_autocmd("PackChanged", {
+        callback = function(ev)
+            local name, kind = ev.data.spec.name, ev.data.kind
+            if name == "fff.nvim" and (kind == "install" or kind == "update") then
+                if not ev.data.active then
+                    vim.cmd.packadd("fff.nvim")
+                end
+                require("fff.download").download_or_build_binary()
             end
-            require("fff.download").download_or_build_binary()
-        end
-    end,
-})
-
-vim.g.fff = {
-    lazy_sync = true,
-    debug = {
-        enabled = true,
-        show_scores = true,
-    },
-}
-
-local fff_ok = pcall(require, "fff")
-if fff_ok and not is_vscode then
-    vim.keymap.set("n", "ff", function()
-        require("fff").find_files()
-    end, { desc = "fff files" })
-    -- you can toggle between grep, fuzzy grep, regex grep with shift+tab after launching fff grep
-    vim.keymap.set("n", "fg", function()
-        require("fff").live_grep()
-    end, { desc = "fff grep" })
+        end,
+    })
+end)
+if fff_ok then
+    vim.g.fff = {
+        lazy_sync = true,
+        debug = {
+            enabled = true,
+            show_scores = true,
+        },
+    }
+    if not is_vscode then
+        vim.keymap.set("n", "ff", function()
+            require("fff").find_files()
+        end, { desc = "fff files" })
+        -- you can toggle between grep, fuzzy grep, regex grep with shift+tab after launching fff grep
+        vim.keymap.set("n", "fg", function()
+            require("fff").live_grep()
+        end, { desc = "fff grep" })
+    end
 end
 
 -- gitsigns
